fc699b020d67ea0eed498358ef4abdbd76dde3c5
Earth287 <erin007ster@gmail.com>
2016-10-19 07:49:20 -0400

    completed JS tip calc

---
+++ main.js
@@ -0,0 +1,11 @@
+var bill_amount = Number(prompt('What is your bill amount?'));
+function tip(bill_amount){
+  tip_amount = (bill_amount*0.2)
+  return total_bill = tip_amount + bill_amount
+};
+
+console.log(alert("Your total bill, including tip, is " + ('$' + tip(bill_amount)) + "."));
+
+//I also had this initially console.log the final bill amount sentence. I am not sure if there
+//is an easy way to have it print on the console AND be an alert, or if that would even be necessary.
+//Overall, writing the funtion came back to me pretty quickly which was exciting!
